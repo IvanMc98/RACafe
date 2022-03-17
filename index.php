94b92eaaf8018a703c359d2a0175a2da0e7d3c3f
Ivan Montoya <moty5871@gmail.com>
2022-03-17 15:21:22 -0600

    Cambio de formato

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+echo '<!doctype html>';
+echo '<html lang="en">';
+echo '<head>';
+echo '<title>&lt;model-viewer&gt; template</title>';
+echo '<meta charset="utf-8">';
+echo '<meta name="description" content="&lt;model-viewer&gt; template">';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
+echo '<link type="text/css" href="./styles.css" rel="stylesheet"/>';
+echo '<!-- OPTIONAL: The :focus-visible polyfill removes the focus ring for some input types -->';
+echo '<script src="https://unpkg.com/focus-visible@5.0.2/dist/focus-visible.js" defer></script>';
+echo '</head>';
+echo '<body>';
+echo '<!-- <model-viewer> HTML element -->';
+echo '<model-viewer bounds="tight" src="Bolsa.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls environment-image="neutral" poster="poster.webp" shadow-intensity="2" shadow-softness="1" exposure="0.49">';
+echo '<div class="progress-bar hide" slot="progress-bar">';
+echo '<div class="update-bar"></div>';
+echo '</div>';
+echo '<button slot="ar-button" id="ar-button">';
+echo 'View in your space';
+echo '</button>';
+echo '<div id="ar-prompt">';
+echo '<img src="ar_hand_prompt.png">';
+echo '</div>';
+echo '</model-viewer>';
+echo '<script src="script.js"></script>';
+echo '<!-- Loads <model-viewer> for browsers: -->';
+echo '<script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>';
+echo '</body>';
+echo '</html>';
+?>
